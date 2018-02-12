9ec4c403e64bcb030df51d1d9ad1dbab96b20a6a
aodren35 <aodren.letellier@etudiant.univ-rennes1.fr>
2018-02-12 12:30:57 +0100

    package name

--- app/src/main/java/com/mmm/pingmeat/MainActivity.java
+++ app/src/main/java/com/mmm/pingmeat/MainActivity.java
@@ -3,8 +3,6 @@ package com.mmm.pingmeat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.mmm.pingmeat.mmm_project.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
