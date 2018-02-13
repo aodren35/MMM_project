839694ecf85ccf1daea8e8ff14397244c5c01610
bjbprudor <bastienprudor@gmail.com>
2018-02-13 19:15:06 +0100

    Ajout de models objet pour la database firebase

--- app/src/main/java/com/mmm/pingmeat/models/FoodType.java
+++ app/src/main/java/com/mmm/pingmeat/models/FoodType.java
@@ -1,8 +1,22 @@
 package com.mmm.pingmeat.models;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
 /**
  * Created by Murakumo on 13/02/2018.
  */
+@IgnoreExtraProperties
+public class FoodType
+{
+    public Integer foodtype_id;
+    public String name;
 
-public class FoodType {
+    public FoodType() {
+
+    }
+
+    public FoodType(Integer foodtype_id, String name) {
+        this.foodtype_id = foodtype_id;
+        this.name = name;
+    }
 }
